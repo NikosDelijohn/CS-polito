04c5bc99aebf2c2fb0c56dd19e33836d8c343a0b
Nick <nd.serv.acc@gmail.com>
2022-12-04 23:47:38 +0100

    Update ex1-1.py

--- lab3/ex1-1.py
+++ lab3/ex1-1.py
@@ -3,53 +3,58 @@
 
 from math import sqrt
 
-# Comparison I
-if 1 == 1:
-    print("1 == 1? True")
-else:
-    print("1 == 1? False")
+def main(): 
+    
+    # Comparison I
+    if 1 == 1:
+        print("1 == 1? True")
+    else:
+        print("1 == 1? False")
 
-# Comparison II
-if 1 == 1.0:
-    print("1 == 1.0? True")
-else:
-    print("1 == 1.0? False")
+    # Comparison II
+    if 1 == 1.0:
+        print("1 == 1.0? True")
+    else:
+        print("1 == 1.0? False")
 
-# Comparison III
-if 2.0 == sqrt(4.0):
-    print("2.0 == sqrt(4.0)? True")
-else:
-    print("2.0 == sqrt(4.0)? False")
+    # Comparison III
+    if 2.0 == sqrt(4.0):
+        print("2.0 == sqrt(4.0)? True")
+    else:
+        print("2.0 == sqrt(4.0)? False")
 
-# Comparison IV
-if '1' == 1:
-    print("'1' == 1? True")
-else:
-    print("'1' == 1? False")
+    # Comparison IV
+    if '1' == 1:
+        print("'1' == 1? True")
+    else:
+        print("'1' == 1? False")
 
-# Comparison V
-if 'ciao' == 'Ciao':
-    print("'ciao' == 'Ciao'? True")
-else:
-    print("'ciao' == 'Ciao'? False")
+    # Comparison V
+    if 'ciao' == 'Ciao':
+        print("'ciao' == 'Ciao'? True")
+    else:
+        print("'ciao' == 'Ciao'? False")
 
 
 
-# ALTERNATE SOLUTION: the result of a comparison (e.g. 1==1) is itslef a boolean value,
-# and can be memorized in a variable. Said variable can be either True or False
-# (predefined constants in Python) and, as such, can be printed
+    # ALTERNATE SOLUTION: the result of a comparison (e.g. 1==1) is itslef a boolean value,
+    # and can be memorized in a variable. Said variable can be either True or False
+    # (predefined constants in Python) and, as such, can be printed
 
-comparison_I = (1 == 1)
-print("1 == 1?", comparison_I)
+    comparison_I = (1 == 1)
+    print("1 == 1?", comparison_I)
 
-comparison_II = (1 == 1.0)
-print("1 == 1.0?", comparison_II)
+    comparison_II = (1 == 1.0)
+    print("1 == 1.0?", comparison_II)
 
-comparison_III = (2.0 == sqrt(4.0))
-print("2.0 == sqrt(4.0)?", comparison_III)
+    comparison_III = (2.0 == sqrt(4.0))
+    print("2.0 == sqrt(4.0)?", comparison_III)
 
-comparison_IV = ('1' == 1)
-print("'1' == 1?", comparison_IV)
+    comparison_IV = ('1' == 1)
+    print("'1' == 1?", comparison_IV)
 
-comparison_V = ('ciao' == 'Ciao')
-print("'ciao' == 'Ciao'?", comparison_V)
+    comparison_V = ('ciao' == 'Ciao')
+    print("'ciao' == 'Ciao'?", comparison_V)
+
+if __name__ == "__main__":
+    main()
