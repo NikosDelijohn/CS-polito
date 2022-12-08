7fa6128a1aafa472a6dcf817e1c33a50a19cff1e
delijohn <nd.serv.acc@gmail.com>
2022-12-08 23:44:33 +0100

    Patched erroneous code (Issue #1)

--- lab04/ex1-8.py
+++ lab04/ex1-8.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3 
 
+
+
 def main():
-   
-    ok = True
-    found_equals = False
-    prev = -1
-    repeated = ""
 
-    while ok:
-        line = input("Write a number: ")
-        if line == "":
-            ok = False
-        else:
-            if not line.isdigit():
-                print("This should be a number!")
-            else:
-                num = int(line)
-                if num == prev:
-                    found_equals = True
-                if num != prev and found_equals == True:
-                    repeated += f"{prev} "
-                    found_equals = False
-                prev = num
-    print(repeated)
+    sequence = list() 
 
+    # fill the sequence 
+    while True: 
+
+        user_input = input("Give me an integer number: ")
+        while user_input and not user_input.isdigit(): 
+            user_input = input("[SANITY CHECK FAILED] Give me an integer number: ")
+
+        if not user_input: 
+            break 
+
+        sequence.append(int(user_input))
+
+    # scan for dups 
+    duplicates = list() 
+    for index in range(len(sequence)-1):
+
+        if (sequence[index] == sequence[index+1]) \
+        and(sequence[index] not in duplicates):
+            duplicates.append(sequence[index])      
+
+    # report duplicates 
+    for dup in duplicates: 
+        print(dup,end=" ")
+    print()
 
 if __name__ == "__main__":
     main()
